0cad880bb1214ba34f7c8e427344a328ac91f919
SDRoze <nuntawatrachakom@gmail.com>
2019-08-21 04:43:26 +0700

    Initial commit

---
+++ app/src/main/java/dev/ecommerce/eshopping/Prevalent/Prevalent.java
@@ -0,0 +1,10 @@
+package dev.ecommerce.eshopping.Prevalent;
+
+import dev.ecommerce.eshopping.Model.User;
+
+public class Prevalent {
+    private static User currentOnilneUser;
+
+    public static final String UserPhonekey = "UserPhone";
+    public static final String UserPasswordkey = "UserPassword";
+}
